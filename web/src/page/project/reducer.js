e28986141e30395d5664204f125a5e549c3f798f
wwwfuqiang721 <fuqiangwang721@163.com>
2018-12-22 14:15:12 +0800

    缩进修改~测试

--- web/src/page/project/reducer.js
+++ web/src/page/project/reducer.js
@@ -3,14 +3,14 @@ import * as actionType from './actionType';
 
 
 
-function resolveupdateFile(state = {}, action){
-    switch(action.type) {
-        case actionType.RESOVE_UPDATE_USERMSGFILE:
-        return action.payload.data; 
-        default: 
-        return state
-    }
+function resolveupdateFile(state = {}, action) {
+	switch (action.type) {
+		case actionType.RESOVE_UPDATE_USERMSGFILE:
+			return action.payload.data;
+		default:
+			return state
+	}
 }
 export default combineReducers({
-    updateFile: resolveupdateFile
+	updateFile: resolveupdateFile
 });
