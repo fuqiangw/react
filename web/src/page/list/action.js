2cda8e47cf17a0c33b6960f78c905799b398925d
wwwfuqiang721 <fuqiangwang721@163.com>
2018-12-22 13:46:04 +0800

    代码格式,缩进修改

--- web/src/page/list/action.js
+++ web/src/page/list/action.js
@@ -2,17 +2,17 @@ import * as actionType from './actionType';
 
 
 export const getProductList = (url, filters) => ({
-    type: actionType.GET_PRODUCT_LIST,
-    payload: {
-        url,
-        filters
-    }
+	type: actionType.GET_PRODUCT_LIST,
+	payload: {
+		url,
+		filters
+	}
 });
 
 
 export const getSearchFilterCond = (url) => ({
-    type: actionType.GET_SEARCH_FILTER_COND,
-    payload: {
-        url,
-    }
+	type: actionType.GET_SEARCH_FILTER_COND,
+	payload: {
+		url,
+	}
 });
